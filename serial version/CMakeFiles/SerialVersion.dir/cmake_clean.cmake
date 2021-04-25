e101ac551ed3479fb84c4968495a6bc28786432b
Xavier Ruiz <xruizify@gmail.com>
2021-04-25 13:09:11 -0400

    add stuf

---
+++ serial version/CMakeFiles/SerialVersion.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SerialVersion.dir/main.cpp.o"
+  "SerialVersion"
+  "SerialVersion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SerialVersion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
